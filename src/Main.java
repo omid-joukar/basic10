186a3cde2f918f59529a722bdfade6d3a86b6226
omid-joukar <58634223+omid-joukar@users.noreply.github.com>
2020-07-03 15:52:53 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by KPS on 7/3/2020.
+ */
+public class Main {
+    public static void main(String[] args) {
+        double a = 4.0 * (1 - (1.0 / 3) + (1.0 / 5) - (1.0 / 7) + (1.0 / 9) - (1.0 / 11));
+        System.out.println(a);
+    }
+}
